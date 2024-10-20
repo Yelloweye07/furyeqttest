9c2822402e36525ac83e40ea31b2a2ea07ca1fd2
Yelloweye07 <125079264+Yelloweye07@users.noreply.github.com>
2024-10-20 13:01:50 +0300

    Add files via upload

--- second_win.py
+++ second_win.py
@@ -1,11 +1,53 @@
-from PyQt5.QtCore import Qt,QTimer
+
+from PyQt5.QtCore import Qt, QTimer, QTime
 from PyQt5.QtWidgets import (
         QApplication, QWidget, 
         QHBoxLayout, QVBoxLayout,
         QPushButton, QLabel,QLineEdit)
 from instr import * 
+from final_win import *
 
 class SecondWin(QWidget):
+    def timer_test(self):
+       global time 
+       time = QTime(0, 1, 0)
+       self.timer = QTimer()
+       self.timer.timeout.connect(self.timer1Event)
+       self.timer.start(1000)
+    def timer1Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.timerLabel.setText(time.toString("hh:mm:ss"))
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
+    def timer_sits(self):
+        global time 
+        time = QTime(0, 0, 30)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer2Event)
+        self.timer.start(1500)
+    def timer2Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.timerLabel.setText(time.toString("hh:mm:ss")[6:8])
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
+    def timer_final(self):
+       global time 
+       time = QTime(0, 1, 0)
+       self.timer = QTimer()
+       self.timer.timeout.connect(self.timer3Event)
+       self.timer.start(1000)
+    def timer3Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.timerLabel.setText(time.toString("hh:mm:ss"))
+        if int(time.toString("hh:mm:ss")[6:8]) >= 45:
+            self.timerLabel.setStyleSheet("color: rgb(0,255,0); font: bold 50px")
+        elif int(time.toString("hh:mm:ss")[6:8]) <= 15:
+            self.timerLabel.setStyleSheet("color: rgb(0,255,0); font: bold 50px")
+        else:
+            self.timerLabel.setStyleSheet("color: rgb(0,0,0); font: bold 50px")
     def set_appear(self):
         pass
     def initUI(self):
@@ -23,8 +65,8 @@ class SecondWin(QWidget):
         self.test2hint = QLineEdit(txt_hinttest2)
         self.test3hint = QLineEdit(txt_hinttest3)
         self.sendresults = QPushButton(txt_sendresults)
-        self.timer = QLabel('00:00:00')
-        self.timer.setStyleSheet("font: bold 50px")
+        self.timerLabel = QLabel('00:00:00')
+        self.timerLabel.setStyleSheet("font: bold 50px")
         self.horizontal_line = QHBoxLayout()
         self.right_line = QVBoxLayout()
         self.left_line = QVBoxLayout()
@@ -42,18 +84,25 @@ class SecondWin(QWidget):
         self.left_line.addWidget(self.test2hint,alignment = Qt.AlignLeft)
         self.left_line.addWidget(self.test3hint,alignment = Qt.AlignLeft)
         self.left_line.addWidget(self.sendresults,alignment = Qt.AlignLeft)
-        self.right_line.addWidget(self.timer)
+        self.right_line.addWidget(self.timerLabel)
         self.horizontal_line.addLayout(self.left_line)
         self.horizontal_line.addLayout(self.right_line)
         self.setLayout(self.horizontal_line)
     def connects(self):
-        pass
+        self.sendresults.clicked.connect(self.next_click)
+        self.starttest1.clicked.connect(self.timer_test)
+        self.starttest2.clicked.connect(self.timer_sits)
+        self.starttest3.clicked.connect(self.timer_final)
     def __init__ (self):
         super().__init__()
         self.set_appear()
         self.initUI()
         self.connects()
         self.show()
+    def next_click(self):
+        self.hide()
+        self.tw = WinThree()
+
 app = QApplication([])
-sw = SecondWin()
+mw = SecondWin()
 app.exec_()
